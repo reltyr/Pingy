f216d17a2940c167a7af704630c32712643691f0
müjde özden <emrehacker111@gmail.com>
2025-01-04 17:50:17 +0300

    Added some new features along with bugfixes

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMembers
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages
     ]
 });
 
 const MAX_PINGS = 100;
+const CD_MS = 60000;
+
 const cooldowns = new Collection();
 
 const commands = [
@@ -39,41 +42,53 @@ const commands = [
                 choices: [
                     {
                         name: 'Server',
-                        value: 'server'
+                        value: 'this server'
                     },
                     {
-                        name: 'DM',
-                        value: 'dm'
+                        name: 'Direct Message',
+                        value: 'DMs'
                     }
                 ]
+            },
+            {
+                name: 'context',
+                description: 'Additional message to send with the ping',
+                type: 3,
+                required: false
             }
-        ]
+        ]   
     }
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
-try {
-    console.log('Started refreshing application (/) commands.');
-    await rest.put(
-        Routes.applicationCommands(process.env.CLIENT_ID),
-        { body: commands }
-    );
-} catch (error) {
-    console.error(error);
-}
+(async () => {
+    try {
+        console.log('\nStarted refreshing application (/) commands.\n');
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: commands }
+        );
+        console.log('Successfully reloaded application (/) commands.\n');
+    } catch (err) {
+        console.error('Error reloading application (/) commands:', err , '\n');
+    }
+})();
 
 client.once('ready', () => {
-    console.log(`Successfully logged in as { ${client.user.tag} } !!!`);
+    console.log(`Successfully logged in as { ${client.user.tag} } !\n\n`);
 });
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
     if (interaction.commandName === 'ping') {
+
         const cooldown = cooldowns.get(interaction.user.id);
+
         if (cooldown) {
             const remaining = cooldown - Date.now();
+
             if (remaining > 0) {
                 return interaction.reply({
                     content: `You're on cooldown. Please wait ${Math.ceil(remaining / 1000)} seconds before using this command again.`,
@@ -84,7 +99,29 @@ client.on('interactionCreate', async interaction => {
 
         const target = interaction.options.getUser('user');
         const amount = interaction.options.getInteger('amount');
+        const context = interaction.options.getString('context');
         const method = interaction.options.getString('method');
+
+        if (target.id === client.user.id) {
+            return interaction.reply({
+                content: 'I cannot ping myself!',
+                flags: MessageFlags.Ephemeral
+            });
+        }
+
+        if (!target) {
+            return interaction.reply({
+                content: 'User not found',
+                flags: MessageFlags.Ephemeral
+            });
+        }
+
+        if (interaction.guild === null) {
+            return interaction.reply({
+                content: 'This command can only be used in a server.',
+                flags: MessageFlags.Ephemeral
+            });
+        }
 
         const confirm = new ButtonBuilder()
             .setCustomId('confirm')
@@ -100,7 +137,7 @@ client.on('interactionCreate', async interaction => {
             .addComponents(confirm, cancel);
         
         const response = await interaction.reply({
-            content: `Are you sure you want to ping ${target} ${amount} time(s) through ${method}?`,
+            content: `Are you sure you want to ping ${target} ${amount} time(s) through ${method} ?`,
             components: [row],
             flags: MessageFlags.Ephemeral
         });
@@ -109,54 +146,63 @@ client.on('interactionCreate', async interaction => {
             const confirmation = await response.awaitMessageComponent({ time: 30000 });
 
             if (confirmation.customId === 'confirm') {
-                cooldowns.set(interaction.user.id, Date.now() + 60000);
+                cooldowns.set(interaction.user.id, Date.now() + CD_MS);
 
                 await confirmation.update({
-                    content: `Pinging ${target} ${amount} time(s) through ${method}...`,
+                    content: `Attempting to ping ${target} ${amount} time(s) through ${method} ...`,
                     components: [],
                     flags: MessageFlags.Ephemeral
                 });
 
-                for (let i = 0; i < amount; i++) {
-                    if (method === 'dm') {
-                        try {
-                            await target.send(`${target}, you have been pinged by ${interaction.user} from ${interaction.guild.name}!`);
-                        } catch (error) {
-                            await interaction.followUp({
-                                content: `Failed to DM ${target}, They might have DMs disabled.`,
-                                flags: MessageFlags.Ephemeral
-                            });
-                            break;
+                let success = true;
+
+                if (method === 'DMs') {
+                    try {
+                        for (let i = 0; i < amount; i++) {
+                            await target.send(`${target}, you have been pinged by ${interaction.user} from ${interaction.guild.name} !\n${context ? ` {  ${context}  }` : ''}`);
+
+                            await new Promise(resolve => setTimeout(resolve, 300));
                         }
-                    } else {
-                        await interaction.channel.send(`${target}`);
+                    } catch (err) {
+                        await interaction.followUp({
+                            content: `Failed to DM ${target}, they might have DMs disabled.`,
+                            flags: MessageFlags.Ephemeral
+                        });
+
+                        console.error('Error sending DM:', err , '\n');
+                        success = false;
                     }
-
-                    await new Promise(resolve => setTimeout(resolve, 300));
-
+                } else {
+                    for (let i = 0; i < amount; i++) {
+                        await interaction.channel.send(`${target}, You have been pinged!\n${context ? ` {  ${context}  } ` : ''}`);
+                        
+                        await new Promise(resolve => setTimeout(resolve, 300));
+                    }
                 }
 
                 await interaction.followUp({
-                    content: `Successfully pinged ${target} ${amount} time(s) through ${method}!`,
+                    content: success
+                        ? `Successfully pinged ${target} ${amount} time(s) through ${method}!`
+                        : `Failed to ping ${target} ${amount} time(s) through ${method}!`,
                     flags: MessageFlags.Ephemeral
                 });
-            } else {
 
+            } else {
                 await confirmation.update({
                     content: 'Command cancelled.',
+                    withResponse: true,
                     components: [],
                     flags: MessageFlags.Ephemeral
                 });
-
             }
-        } catch (e) {
-
+        } catch (err) {
             await interaction.editReply({
-                content: 'No response after 30 seconds, command cancelled...',
+                content: 'Command timed out.',
                 components: [],
                 flags: MessageFlags.Ephemeral
             });
 
+            console.error('Error awaiting message component:', err , '\n'); //
         }
     }
 });
